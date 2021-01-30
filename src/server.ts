904b8431e12cf91fd972695d30b7cb46328bbb6b
sitek94 <msitkowski94@gmail.com>
2021-01-30 18:20:33 +0100

    add basic setup

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
 
+const app = express();
 
+// Middlewares
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('public'));
+
+// Homepage route
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('views/index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
