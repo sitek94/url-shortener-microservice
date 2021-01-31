88b7da0911174a292e77672e9ee636c112b87116
sitek94 <msitkowski94@gmail.com>
2021-01-31 14:00:53 +0100

    update '/new' route; add '/:short_url' route

--- src/routes/api/shorturl.ts
+++ src/routes/api/shorturl.ts
@@ -6,6 +6,15 @@ const router = Router();
 
 router.post('/new', verifyUrl, async (req, res) => {
   try {
+    // Check if url is already in the db
+    const item = await Item.findOne({ original_url: req.body.url });
+    if (item) {
+      return res.json({
+        original_url: item.original_url,
+        short_url: item.short_url,
+      });
+    }
+
     // Count the docs
     const count = await Item.estimatedDocumentCount();
     if (!count) throw Error(`Failed to count the docs`);
@@ -23,9 +32,22 @@ router.post('/new', verifyUrl, async (req, res) => {
     if (!savedItem) throw Error(`Failed to save new item`);
 
     // Send response
-    res.json(urlObj);
+    return res.json(urlObj);
   } catch (e) {
-    res.json({ msg: e.message });
+    return res.json({ msg: e.message });
+  }
+});
+
+router.get('/:short_url', async (req, res) => {
+  try {
+    // Find item by short url in the database
+    const item = await Item.findOne({ short_url: req.params.short_url });
+    if (!item) throw Error(`No short URL found for the given input`);
+
+    // Redirect to the original url
+    return res.redirect(item.original_url);
+  } catch (e) {
+    return res.json({ error: e.message });
   }
 });
 
