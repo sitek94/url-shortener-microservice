779eda3c1209716b1e2fa6e7f0907ff1e8d7b8b5
sitek94 <msitkowski94@gmail.com>
2021-02-12 21:14:44 +0100

    update secrets

--- src/config/secrets.ts
+++ src/config/secrets.ts
@@ -3,13 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { NODE_ENV, MONGO_URI_DEV, MONGO_URI_PROD, MONGO_URI_TEST } = process.env;
+const { NODE_ENV, MONGO_URI_DEV, MONGO_URI_PROD } = process.env;
 
 export const MONGO_URI = (NODE_ENV === 'production'
   ? MONGO_URI_PROD
-  : NODE_ENV === 'development'
-  ? MONGO_URI_DEV
-  : MONGO_URI_TEST) as string;
+  : MONGO_URI_DEV) as string;
 
 if (!MONGO_URI) {
   console.log(
