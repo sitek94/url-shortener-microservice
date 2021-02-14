cedc1f35732bde71dedde1ef23c119681f0c0ec2
sitek94 <msitkowski94@gmail.com>
2021-02-14 12:05:42 +0100

    ✅ update tests

--- test/validate-url.test.ts
+++ test/validate-url.test.ts
@@ -1,15 +1,43 @@
 import { validateUrl } from '../src/util/validate-url';
 
-describe('urlValidator', () => {
-  it('throws when called with empty string', async () => {
-    expect(validateUrl('')).rejects.toThrow();
+describe('validateUrl function', () => {
+  it(`throws when called with empty string`, async () => {
+    try {
+      await validateUrl('');
+    } catch (e) {
+      expect(e.message).toMatch(/invalid url/i);
+    }
   });
 
-  it(`throws when url doesn't start with "http(s)"`, async () => {
-    expect(validateUrl('www.maciek.com')).rejects.toThrow();
+  it(`throws when called with a string that doesn't start with "http(s)"`, async () => {
+    try {
+      await validateUrl('www.maciek.com');
+    } catch (e) {
+      expect(e.message).toMatch(/invalid url/i);
+    }
+  });
+
+  it(`throws when called with the site's URL`, async () => {
+    try {
+      await validateUrl('https://url-shortener.com');
+    } catch (e) {
+      expect(e.message).toMatch(/invalid url/i);
+    }
+  });
+
+  it(`throws when called with localhost URL`, async () => {
+    try {
+      await validateUrl('http://localhost:5000');
+    } catch (e) {
+      expect(e.message).toMatch(/invalid url/i);
+    }
   });
 
   it(`throws on dns lookup failure`, async () => {
-    expect(validateUrl('https://incorrect-hostname.com')).rejects.toThrow();
+    try {
+      await validateUrl('https://incorrect-hostname.com');
+    } catch (e) {
+      expect(e.message).toMatch(/invalid url/i);
+    }
   });
 });
