ae9a65a5986ed38577c1c2b8194d6d7ab19aeeb2
sitek94 <msitkowski94@gmail.com>
2021-02-14 12:17:45 +0100

    ✅ fix tests, closes #3

--- test/url-validator.test.ts
+++ test/url-validator.test.ts
@@ -1,16 +1,21 @@
 import request from 'supertest';
 import app from '../src/app';
-import { urlValidator } from '../src/middleware/url-validator';
 
 describe('validateUrl', () => {
   it('catches invalid URL', async () => {
-    try {
-      await request(app).post('/api/shorturl/new').send({
-        url: 'asdfasfda',
-      });
-    } catch (e) {
-      expect(e).toBeTruthy();
-      expect(urlValidator).toHaveBeenCalled();
-    }
+    const res = await request(app).post('/api/shorturl/new').send({
+      url: 'afdsasfas',
+    });
+
+    expect(res.body.error).toMatch(/invalid url/i);
+  });
+
+  it('lets valid URL through', async () => {
+    const res = await request(app).post('/api/shorturl/new').send({
+      url: 'https://google.com',
+    });
+
+    expect(res.status).toBe(200);
+    expect(res.body.short_url).toBeTruthy();
   });
 });
